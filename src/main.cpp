b27d80117815ac34b565f43bee0b6ff9b0ae82df
Chris Thrasher <chrisjthrasher@gmail.com>
2023-05-10 12:41:32 -0600

    Demonstrate how to acquire and use ImGui-SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,37 @@
 #include <SFML/Graphics.hpp>
+#include <imgui-SFML.h>
+#include <imgui.h>
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
+    if (!ImGui::SFML::Init(window))
+        return -1;
 
+    sf::Clock clock;
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+            ImGui::SFML::ProcessEvent(window, *event);
+
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
             }
         }
 
+        ImGui::SFML::Update(window, clock.restart());
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
         window.clear();
+        ImGui::SFML::Render(window);
         window.display();
     }
+
+    ImGui::SFML::Shutdown();
 }
