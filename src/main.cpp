5014143be63294a255cee39eb0a424203fceabb7
Chris Thrasher <chrisjthrasher@gmail.com>
2024-10-13 14:40:30 -0600

    Use SFML style conventions

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        for (auto event = sf::Event(); window.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed)
             {
