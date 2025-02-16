98720dad725d81a27db66ed30a8ebb26584dde64
Cameron Przybylski <cameronprzybylski@yahoo.com>
2025-02-16 14:10:42 -0600

    Testing setup

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,13 @@ int main()
         }
 
         window.clear();
+        //sf::Texture texture("1162.jpg", false, sf::IntRect({10, 10}, {32, 32})); // Throws sf::Exception if an error occurs
+        //sf::Texture texture("1162.jpg");
+        //sf::Sprite sprite(texture);
+        //window.draw(sprite);
+        sf::CircleShape shape(50.f);
+        shape.setFillColor(sf::Color(100, 250, 50));
+        window.draw(shape);
         window.display();
     }
 }
