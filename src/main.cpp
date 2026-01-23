2c7e392d215a8185669577d0200f696464fa9817
Jonny Paton <jonathan.r.paton@googlemail.com>
2026-01-23 11:41:24 +0000

    Use classic green circle code

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,20 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
+	sf::CircleShape shape( 100.f );
+	shape.setFillColor( sf::Color::Green );
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+	while ( window.isOpen() )
+	{
+		while ( const std::optional event = window.pollEvent() )
+		{
+			if ( event->is<sf::Event::Closed>() )
+				window.close();
+		}
 
-        window.clear();
-        window.display();
-    }
+		window.clear();
+		window.draw( shape );
+		window.display();
+	}
 }
