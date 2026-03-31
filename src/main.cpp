411520d7d308dff044d413c7ae58edd9c8a98f2a
PiTViPeR5687 <cardwell.codyj@gmail.com>
2026-03-30 18:09:33 -0700

    Update main.cpp
    Red

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
 	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+	shape.setFillColor( sf::Color::Red );
 
 	while ( window.isOpen() )
 	{
