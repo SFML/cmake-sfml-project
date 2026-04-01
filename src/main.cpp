eb1a45b502a8ebd68222a729ec9622ea9ae3aee4
PiTViPeR5687 <cardwell.codyj@gmail.com>
2026-03-31 22:14:46 -0400

    Added FPS control. Space makes square yellow. Window functionality present.

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,10 @@
 int main()
 {
 	sf::RenderWindow window;
+	sf::Clock mainFPSClock;
 	windowInit( window, 768, 1024, 60, false, false );
 	sf::Vector2f windowSize(window.getSize().x, window.getSize().y);
+
 
 	/////TEST SHAPE SHIT/////
 		sf::RectangleShape shape(sf::Vector2f(100.f, 100.f));
@@ -15,23 +17,27 @@ int main()
 		shape.setOrigin(shape.getGeometricCenter());
 		shape.setPosition( windowSize / 2.f );
 	//////////////////////////
-
+	mainFPSClock.restart();
 	while ( window.isOpen() )
 	{
 		while ( const std::optional event = window.pollEvent() )
 		{
 			if ( event->is<sf::Event::Closed>() )
 				window.close();
+			if ( event->is<sf::Event::KeyPressed>() )
+				shape.setFillColor( sf::Color::Yellow );
 		}
+		if (mainFPSClock.getElapsedTime().asMilliseconds() >= 1000) {
+			/////MORE TEST SHAPE SHIT///////////
+			const sf::Angle angle1 = shape.getRotation();
+			//shape.rotate(angle1 + sf::degrees(.0002f));
+			shape.setRotation(angle1 + sf::degrees(.2f));
 
-	/////MORE TEST SHAPE SHIT///////////
-		const sf::Angle angle1 = shape.getRotation();
-		shape.rotate(angle1 + sf::degrees(.000002f));
+			/////////////////////////////
 
-	/////////////////////////////
-
-		window.clear(sf::Color::Blue);
-		window.draw( shape );
-		window.display();
+			window.clear(sf::Color::Blue);
+			window.draw( shape );
+			window.display();
+		}
 	}
 }
