46bca152fe70e75542cc4c891f1532e74a945709
PiTViPeR5687 <cardwell.codyj@gmail.com>
2026-03-31 22:27:32 -0400

    useless cube color changing

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 int main()
 {
+	//VARIABLES AND INITIALIZATION
 	sf::RenderWindow window;
 	sf::Clock mainFPSClock;
 	windowInit( window, 768, 1024, 60, false, false );
@@ -16,28 +17,44 @@ int main()
 		shape.setFillColor( sf::Color::Magenta );
 		shape.setOrigin(shape.getGeometricCenter());
 		shape.setPosition( windowSize / 2.f );
+
 	//////////////////////////
+	//START CLOCK B4 ENTER MAIN LOOP FOR FPS
+	////////////////////////
 	mainFPSClock.restart();
 	while ( window.isOpen() )
 	{
+		//////EVENT HANDLING//////////
+		/////////////////////////
 		while ( const std::optional event = window.pollEvent() )
 		{
 			if ( event->is<sf::Event::Closed>() )
 				window.close();
-			if ( event->is<sf::Event::KeyPressed>() )
+			if ( event->is<sf::Event::KeyPressed>() && shape.getFillColor() == sf::Color::Magenta ) {
 				shape.setFillColor( sf::Color::Yellow );
+				break;
+			}
+			if ( event->is<sf::Event::KeyPressed>() && shape.getFillColor() == sf::Color::Yellow )
+				shape.setFillColor( sf::Color::Magenta );
+				break;
 		}
-		if (mainFPSClock.getElapsedTime().asMilliseconds() >= 1000) {
-			/////MORE TEST SHAPE SHIT///////////
-			const sf::Angle angle1 = shape.getRotation();
-			//shape.rotate(angle1 + sf::degrees(.0002f));
-			shape.setRotation(angle1 + sf::degrees(.2f));
 
-			/////////////////////////////
+		////FPS CONTROL/////////
+		//////////////////////
+			if (mainFPSClock.getElapsedTime().asMilliseconds() >= 1000) {
 
-			window.clear(sf::Color::Blue);
-			window.draw( shape );
-			window.display();
-		}
+				/////MORE TEST SHAPE SHIT///////////
+				//////////////////////////////////
+				const sf::Angle angle1 = shape.getRotation();
+				//shape.rotate(angle1 + sf::degrees(.0002f));
+				shape.setRotation(angle1 + sf::degrees(.2f));
+
+				/////////////////////////////
+				///Window Functions///////
+				/////////////////////////
+				window.clear(sf::Color::Blue);
+				window.draw( shape );
+				window.display();
+			}
 	}
 }
